842a02e254e25a39a1a2b0fbf5a6567f893f0661
Gaëtan Rizio <gaetan@rizio.fr>
2015-05-29 16:47:20 +0200

    Merge with branch V1

--- script/evolutions/7.sql
+++ script/evolutions/7.sql
@@ -1,3 +1,12 @@
+CREATE TABLE menu
+(
+    id SERIAL PRIMARY KEY NOT NULL,
+    lang VARCHAR(10) NOT NULL,
+    role VARCHAR(255) NOT NULL,
+    json VARCHAR NOT NULL
+);
+ALTER TABLE menu ADD CONSTRAINT u_lang_role UNIQUE (lang, role);
+
 INSERT INTO menu(lang, role, json) values ('fr', 'admin', '[]');
 INSERT INTO menu(lang, role, json) values ('fr', 'all', '[]');
 INSERT INTO menu(lang, role, json) values ('en', 'admin', '[]');
